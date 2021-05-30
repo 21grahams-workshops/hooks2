80a30d00eacf0da3c11aa6e9794267d3e97aa3af
21grahams <graham.kirsh@gmail.com>
2021-05-30 16:34:55 -0600

    added delete functionality using id and toggle for checkbox

--- hooks-todo/src/TodoApp.js
+++ hooks-todo/src/TodoApp.js
@@ -6,6 +6,7 @@ import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
+// import uuid from 'uuid/v4'
 
 function TodoApp() {
   const initialTodos = [
@@ -13,9 +14,20 @@ function TodoApp() {
     { id: 1, task: "Shave", completed: true },
     { id: 1, task: "Wash Car", completed: false },
   ];
+
   const [todos, setTodos] = useState(initialTodos);
   const addTodo = (newTodoText) => {
     setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
+  };
+  const removeTodo = (todoId) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+    setTodos(updatedTodos);
+  };
+  const toggleTodo = (todoId) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
   };
 
   return (
@@ -33,10 +45,14 @@ function TodoApp() {
           <Typography color="inherit">TODOS WITH HOOKS</Typography>
         </Toolbar>
       </AppBar>
-      <Grid container justify='center' style={{ marginTop: '1rem'}}>
+      <Grid container justify="center" style={{ marginTop: "1rem" }}>
         <Grid item xs={11} md={8} lg={4}>
           <TodoForm addTodo={addTodo} />
-          <TodoList todos={todos}></TodoList>
+          <TodoList
+            todos={todos}
+            removeTodo={removeTodo}
+            toggleTodo={toggleTodo}
+          ></TodoList>
         </Grid>
       </Grid>
     </Paper>
--- hooks-todo/src/TodoList.js
+++ hooks-todo/src/TodoList.js
@@ -4,14 +4,21 @@ import Paper from "@material-ui/core/Paper";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 
-function TodoList(props) {
+function TodoList({ todos, removeTodo, toggleTodo }) {
   return (
     <div>
       <Paper>
         <List>
-          {props.todos.map((todo) => (
+          {todos.map(todo => (
             <>
-              <Todo task={todo.task} key={todo.id} completed={todo.completed} />
+              <Todo
+                id={todo.id}
+                task={todo.task}
+                key={todo.id}
+                completed={todo.completed}
+                removeTodo={removeTodo}
+                toggleTodo={toggleTodo}
+              />
               <Divider />
             </>
           ))}
