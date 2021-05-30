0bb28ec6c9704c11f1b80a71e5bd886d538c3f69
21grahams <graham.kirsh@gmail.com>
2021-05-30 14:54:16 -0600

    added TodoList Component

--- hooks-todo/src/TodoApp.js
+++ hooks-todo/src/TodoApp.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
-import { useState } from 'react';
 
 function TodoApp() {
+
+  const initialTodos = [
+    {id: 1, task: 'Clean Fishtank', completed: false},
+    {id: 1, task: 'Shave', completed: true},
+    {id: 1, task: 'Wash Car', completed: false}
+  ]
+  const [todos, setTodos] = useState(initialTodos);
+
   return (
     <Paper style={{
       padding: 0,
@@ -21,6 +29,7 @@ function TodoApp() {
           <Typography color='inherit'>TODOS WITH HOOKS</Typography>
         </Toolbar>
       </AppBar>
+      <TodoList todos={todos}></TodoList>
     </Paper>
   )
 }
