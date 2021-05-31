fe2a0ca7d81e6b9a0ce308fb668f0443692d93bf
21grahams <graham.kirsh@gmail.com>
2021-05-30 21:56:05 -0600

    changed ids and used uuidV4

--- hooks-todo/src/TodoApp.js
+++ hooks-todo/src/TodoApp.js
@@ -6,18 +6,18 @@ import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
-// import uuid from 'uuid/v4'
+import { v4 as uuidv4 } from 'uuid';
 
 function TodoApp() {
   const initialTodos = [
     { id: 1, task: "Clean Fishtank", completed: false },
-    { id: 1, task: "Shave", completed: true },
-    { id: 1, task: "Wash Car", completed: false },
+    { id: 2, task: "Shave", completed: true },
+    { id: 3, task: "Wash Car", completed: false },
   ];
 
   const [todos, setTodos] = useState(initialTodos);
   const addTodo = (newTodoText) => {
-    setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
+    setTodos([...todos, { id: uuidv4(), task: newTodoText, completed: false }]);
   };
   const removeTodo = (todoId) => {
     const updatedTodos = todos.filter((todo) => todo.id !== todoId);
