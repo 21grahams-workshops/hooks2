8509779ee4bee7e6dd2760435c640b691e03b722
21grahams <graham.kirsh@gmail.com>
2021-06-02 23:08:39 -0600

    created a useReducer hook and refactored code to incorperate using state and dispatch

--- hooks-todo/src/context/todos.context.js
+++ hooks-todo/src/context/todos.context.js
@@ -1,5 +1,7 @@
-import React, { createContext } from "react";
-import useTodoState from '../hooks/useTodoState';
+import React, { createContext, useReducer } from "react";
+import todoReducer from "../reducers/todo.reducer.js";
+import useTodoState from "../hooks/useTodoState";
+
 const defaultTodos = [
   { id: 1, task: "Mow the lawn", completed: false },
   { id: 2, task: "Get coding", completed: false },
@@ -7,10 +9,10 @@ const defaultTodos = [
 export const TodosContext = createContext();
 
 export function TodosProvider(props) {
-  const todoStuff = useTodoState(defaultTodos);
+  const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
   return (
-    <TodosContext.Provider value={todoStuff}>
+    <TodosContext.Provider value={{ todos, dispatch }}>
       {props.children}
     </TodosContext.Provider>
-  )
+  );
 }
